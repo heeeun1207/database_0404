0b80c3c08d73b8cee4d4273cbbac95c5371d30c4
jungheeeun <jungheeeun272@gmail.com>
2023-04-04 11:36:29 +0900

    CommonJS 모듈 module 객체 말고 exports 객체로 만들기

---
+++ helloWorld.js
@@ -0,0 +1,9 @@
+//! REPL  read(읽고)  , eval(해석) , print(출력) , loop (입력을 기다린다.) 
+function helloWorld (){
+  console.log('Hello World');
+  helloNode();
+}
+function helloNode(){
+  console.log('Hello Node');
+}
+helloWorld();
